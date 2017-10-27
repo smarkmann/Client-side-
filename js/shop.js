7744a3c7e510ec1a98e46385f6f75574dde3931d
Jesper Bruun Hansen <jesperbbhansen@gmail.com>
2017-10-27 18:36:00 +0200

    show some books

---
+++ js/shop.js
@@ -0,0 +1,48 @@
+$(document).ready(() => {
+
+  const bookList = $("#book-list");
+
+  SDK.Book.findAll((err, books) => {
+    if (err) throw err;
+    books.forEach(book => {
+
+      const bookHtml = `
+        <div class="col-lg-4 book-container">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">${book.title}</h3>
+                </div>
+                <div class="panel-body">
+                    <div class="col-lg-4">
+                        <img src="${book.imgUrl}"/>
+                    </div>
+                    <div class="col-lg-8">
+                      <dl>
+                        <dt>Subtitle</dt>
+                        <dd>${book.subtitle}</dd>
+                        <dt>Publisher</dt>
+                        <dd>${book.publisher}</dd>
+                        <dt>ISBN</dt>
+                        <dd>${book.isbn}</dd>
+                      </dl>
+                    </div>
+                </div>
+                <div class="panel-footer">
+                    <div class="row">
+                        <div class="col-lg-4 price-label">
+                            <p>Kr. <span class="price-amount">${book.price}</span></p>
+                        </div>
+                        <div class="col-lg-8 text-right">
+                            <button class="btn btn-success ">Køb</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+
+      bookList.append(bookHtml);
+    });
+  });
+
+
+});
