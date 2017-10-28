b10a0c3fa36ccca034bcb46eadda36a294ba2e68
Jesper Bruun Hansen <jesperbbhansen@gmail.com>
2017-10-28 13:37:33 +0200

    login stuff

--- js/login.js
+++ js/login.js
@@ -8,7 +8,14 @@ $(document).ready(() => {
     const password = $("#inputPassword").val();
 
     SDK.User.login(email, password, (err, data) => {
-      console.log(err, data);
+      if (err && err.xhr.status === 401) {
+        $(".form-group").addClass("has-error");
+      }
+      else if (err){
+        console.log("BAd stuff happened")
+      } else {
+        window.location.href = "my-page.html";
+      }
     });
 
   });
