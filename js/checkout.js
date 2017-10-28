53b732c51feb2f22951ea8dc49b730eae9b940e7
Jesper Bruun Hansen <jesperbbhansen@gmail.com>
2017-10-28 11:07:40 +0200

    Checkout logic

--- js/checkout.js
+++ js/checkout.js
@@ -2,9 +2,20 @@ $(document).ready(() => {
 
   SDK.User.loadNav();
 
+  const currentUser = SDK.User.current();
   const $modalTbody = $("#basket-tbody");
-  const basket = SDK.Storage.load("basket");
+  const $checkoutActions = $("#checkout-actions");
+  const $nothingInBasketContainer = $("#nothing-in-basket-container");
+  const basket = SDK.Storage.load("basket") || [];
   let total = 0;
+
+  $nothingInBasketContainer.show();
+
+  if(!basket.length){
+    $("#checkout-table-container").hide();
+  } else {
+    $nothingInBasketContainer.hide();
+  }
 
   basket.forEach(entry => {
     let subtotal = entry.book.price * entry.count;
@@ -29,5 +40,19 @@ $(document).ready(() => {
         <td>kr. ${total}</td>
       </tr>
     `);
+
+  if (currentUser) {
+    $checkoutActions.html(`
+      <button class="btn btn-success btn-lg">Checkout</button>
+    `);
+  }
+  else {
+    $checkoutActions.html(`
+      <a href="login.html">
+        <button class="btn btn-primary btn-lg">Log in to checkout</button>
+      </a>
+    `);
+  }
+
 
 });
