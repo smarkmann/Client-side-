b7740b9f96d1401516d230ff81e2774ffd76eed0
smarkmann <soma16ab@student.cbs.dk>
2017-11-06 13:48:09 +0100

    gjort noget med shop

--- js/shop.js
+++ js/shop.js
@@ -1,5 +1,78 @@
 $(document).ready(() => {
+// når man skal finde et ID = #
+// Når man skal finde en klasse = .
 
-  SDK.User.loadNav();
+    SDK.User.loadNav();
+    const $bookList = $("#book-list");
+
+    SDK.Book.findAll((err, books)=> {
+
+        books.forEach((book)=> {
+            const bookHtml = `
+        <div class="col-lg-4 book-container">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">${book.title}</h3>
+                </div>
+                <div class="panel-body">
+                    <div class="col-lg-4">
+                        <img src="${book.imgUrl}"/>
+                    </div>
+                    <div class="col-lg-8">
+                      <dl>
+                        <dt>Subtitle</dt>
+                        <dd>${book.subtitle}</dd>
+                        <dt>Publisher</dt>
+                        <dd>${book.publisher}</dd>
+                        <dt>ISBN</dt>
+                        <dd>${book.isbn}</dd>
+                      </dl>
+                    </div>
+                </div>
+                <div class="panel-footer">
+                    <div class="row">
+                        <div class="col-lg-4 price-label">
+                            <p>Kr. <span class="price-amount">${book.price}</span></p>
+                        </div>
+                        <div class="col-lg-8 text-right">
+                            <button class="btn btn-success purchase-button" data-book-id="${book.id}">Add to basket</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+
+            $bookList.append(bookHtml);
+
+        });
+        $(".purchase-button").click(function() {
+           $("#purchase-modal").modal("toggle");
+            const bookId = $(this).data("book-id");
+            const book = books.find((book) => book.id === bookId);
+            SDK.Book.addToBasket(book)
+                //console.log(bookId);
+
+
+        });
+    });
+    $("#purchase-modal").on("shown.bs.modal", () => {
+        const basket = SDK.Storage.load("basket");
+        const $modalTbody = $("#modal-tbody");
+        basket.forEach((entry) => {
+
+            $modalTbody.append(`
+        <tr>
+            <td>
+                <img src="${entry.book.imgUrl}" height="60"/>
+            </td>
+            <td>${entry.book.title}</td>
+            <td>${entry.count}</td>
+            <td>kr. ${entry.book.price}</td>
+            <td>kr. 0</td>
+        </tr>
+      `);
+        });
+
+        });
 
 });
